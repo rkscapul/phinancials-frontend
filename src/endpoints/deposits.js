68579fb7e6872d19041c0f9b60b57f3c727da10f
ryle <rkscapul@gmail.com>
2022-12-18 00:46:44 +0800

    endpoint update

--- src/endpoints/deposits.js
+++ src/endpoints/deposits.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const deposits = {
   getDepositsSummary: async () => axios.get('http://192.168.2.38:10500/api/deposits/all')
     .then((response) => ({ data: response.data, success: true }))
-    .then({ success: false }),
+    .catch(() => ({ success: false })),
 };
