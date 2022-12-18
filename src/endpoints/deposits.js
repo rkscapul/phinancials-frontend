fd08b9f6a214d21e6dccc6f4f3ec7705b9948e38
ryle <rkscapul@gmail.com>
2022-12-18 16:58:08 +0800

    new endpoints

--- src/endpoints/deposits.js
+++ src/endpoints/deposits.js
@@ -4,4 +4,10 @@ export const deposits = {
   getDepositsSummary: async () => axios.get('http://192.168.2.38:10500/api/deposits/all')
     .then((response) => ({ data: response.data, success: true }))
     .catch(() => ({ success: false })),
+  getAccountDetails: async (accountId) => axios.get(`http://192.168.2.38:10500/api/deposits/${accountId}`)
+    .then((response) => ({ data: response.data, success: true }))
+    .catch(() => ({ success: false })),
+  getTransactions: async (accountId) => axios.get(`http://192.168.2.38:10500/api/deposits/${accountId}/transactions`)
+    .then((response) => ({ data: response.data, success: true }))
+    .catch(() => ({ success: false })),
 };
